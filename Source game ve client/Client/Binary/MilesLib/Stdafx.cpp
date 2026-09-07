1c1aa9cb08a7cee396cb20ceb4b8a05c42890b17
PeregrnS <savas.si82@gmail.com>
2026-09-07 20:13:15 +0300

    Add files via upload

---
+++ Source game ve client/Client/Binary/MilesLib/Stdafx.cpp
@@ -0,0 +1,3 @@
+#include "stdafx.h"
+namespace { char dummy; }; // solve warning lnk4221
+//martysama0134's aad276684955eb3421d3edd3e79cd0dc
---
+++ Source game ve client/Client/Binary/ScriptLib/PythonDebugModule.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void initdbg();
+//martysama0134's aad276684955eb3421d3edd3e79cd0dc
